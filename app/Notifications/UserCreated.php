c1b0ec4c8a902513863fceb7ee8e3e7142ec1ffd
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-10-25 12:19:01 -0300

    Criando notificação de conta de usuário criada

---
+++ app/Notifications/UserCreated.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SON\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserCreated extends Notification
+{
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $appName = config('app.name');
+        return (new MailMessage)
+                    ->subject("Sua conta no $appName foi criada!")
+                    ->greeting("Olá {$notifiable->name}, seja bem-vindo ao $appName")
+                    ->line("Seu número de matrícula é: {$notifiable->enrolment}")
+                    ->line('Obrigado por usar nossa aplicação!!');
+    }
+}
