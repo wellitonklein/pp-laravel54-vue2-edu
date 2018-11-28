3112b39ea52e38893166b00a5546c554ce921a21
wellitoncamposklein <welliton.fokushima@gmail.com>
2018-11-28 12:18:25 -0200

    Criando abas para administração

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,13 +3,17 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>Editar usuário</h3>
-            {!!
-                form($form->add('edit','submit', [
-                    'attr' => ['class' => 'btn btn-primary btn-block'],
-                    'label' => Icon::create('floppy-disk')
-                ]))
-             !!}
+            @component('admin.users.tabs-component', ['user' => $form->getModel()])
+                <div class="col-md-12">
+                    <h3>Editar usuário</h3>
+                    {!!
+                        form($form->add('edit','submit', [
+                            'attr' => ['class' => 'btn btn-primary btn-block'],
+                            'label' => Icon::create('floppy-disk')
+                        ]))
+                     !!}
+                </div>
+            @endcomponent
         </div>
     </div>
 @endsection
--- resources/views/admin/users/profile/edit.blade.php
+++ resources/views/admin/users/profile/edit.blade.php
@@ -3,16 +3,20 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>Editar perfil</h3>
-            <?php $icon = Icon::create('pencil');?>
-            {!!
-                form($form->add('salve', 'submit',
-                    [
-                        'attr' => ['class' => 'btn btn-primary btn-block'],
-                        'label' => $icon
-                    ]
-                ))
-             !!}
+            @component('admin.users.tabs-component', ['user' => $form->getModel()])
+                <div class="col-md-12">
+                    <h3>Editar perfil</h3>
+                    <?php $icon = Icon::create('pencil');?>
+                    {!!
+                        form($form->add('salve', 'submit',
+                            [
+                                'attr' => ['class' => 'btn btn-primary btn-block'],
+                                'label' => $icon
+                            ]
+                        ))
+                     !!}
+                </div>
+            @endcomponent
         </div>
     </div>
 @endsection
